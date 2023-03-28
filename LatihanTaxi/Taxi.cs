3c7adaaabcde47a999c612e449aff2d448c81ea6
RifanKurniawan <mrifan@students.amikom.ac.id>
2023-03-28 20:37:52 +0700

    first commit

---
+++ LatihanTaxi/Taxi.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanTaxi
+{
+    class Taxi
+    {
+        public string DriverName { get; set; }
+        public bool OnDuty { get; set; }
+        public int NumPassenger { get; set; }
+
+        public void TaxiInfo()
+        {
+            Console.WriteLine("Driver Name: {0}", DriverName);
+            if (OnDuty)
+            {
+                Console.WriteLine("On Duty: Yes");
+            }
+            else
+            {
+                Console.WriteLine("On Duty: No");
+            }
+            Console.WriteLine("Number of Passenger: {0}", NumPassenger);
+
+        }
+
+        public void PicUpPassenger()
+        {
+            Console.WriteLine("\n{0} sedang menjemput penumpang", DriverName);
+        }
+
+        public void DropOffPassenger()
+        {
+            Console.WriteLine("{0} selesai mengantar penumpang", DriverName);
+        }
+    }
+}
